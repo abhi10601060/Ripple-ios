e3d879249942e6fa6cb0be4db3ed5cc1abb5dde5
Abhi <abhishekvelekar792000@gmail.com>
2025-11-21 21:02:23 +0530

    initial project added

---
+++ Ripple/Ripple/RippleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RippleApp.swift
+//  Ripple
+//
+//  Created by Abhishek Velekar on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RippleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ripple/RippleTests/RippleTests.swift
@@ -0,0 +1,17 @@
+//
+//  RippleTests.swift
+//  RippleTests
+//
+//  Created by Abhishek Velekar on 21/11/25.
+//
+
+import Testing
+@testable import Ripple
+
+struct RippleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
