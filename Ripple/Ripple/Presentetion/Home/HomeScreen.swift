1298a15602bc8249d0f96741b380b8b365f3eef5
Abhi <abhishekvelekar792000@gmail.com>
2025-12-20 19:49:10 +0530

    Bottom FloatingNavBar Added

---
+++ Ripple/Ripple/Presentetion/Home/HomeScreen.swift
@@ -0,0 +1,61 @@
+//
+//  HomeScreen.swift
+//  Ripple
+//
+//  Created by Abhishek Velekar on 20/12/25.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    
+    @State var selectedTab: String = "Chats"
+    
+    var body: some View {
+        ZStack {
+            VStack(
+                alignment: .trailing
+            ) {
+                HomeScreenHeader
+            }
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity,
+                alignment: .topLeading
+            )
+
+            FloatingBottomNavBar(
+                onSelectionChange: { selectedItem in
+                    self.selectedTab = selectedItem
+                }
+            )
+                .frame(
+                    maxWidth: .infinity,
+                    maxHeight: .infinity,
+                    alignment: .bottom
+                )
+                .padding(.bottom, 10)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.darkBG)
+    }
+
+    var HomeScreenHeader: some View {
+        HStack {
+            CircularImage(imageName: "WhiteRippleLogo", size: 40)
+
+            Text(selectedTab)
+                .font(Font.custom(FontsConstants.Montserrat.rawValue, size: 30))
+                .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, 10)
+    }
+
+}
+
+#Preview {
+    NavigationStack {
+        HomeScreen()
+    }
+}
---
+++ Ripple/Ripple/Presentetion/Shared/CircularImage.swift
@@ -0,0 +1,68 @@
+//
+//  CircularImage.swift
+//  Ripple
+//
+//  Created by Abhishek Velekar on 20/12/25.
+//
+
+import SwiftUI
+
+struct CircularImage: View {
+    
+    private var imageName: String
+    private var size : CGFloat
+    
+    init(imageName: String, size: CGFloat = 100) {
+        self.imageName = imageName
+        self.size = size
+    }
+    
+    var body: some View {
+        if(imageName.starts(with: "https://")){
+            AsyncImage(url: URL(string: imageName)){ phase in
+                
+                if let image = phase.image{
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: size, height: size)
+                        .clipShape(Circle())
+                }
+                else {
+                    Image(systemName: "person.crop.circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: size, height: size)
+                            .clipShape(Circle())
+                            .foregroundColor(Color(.systemGray4))
+                }
+            }
+        }
+        else{
+            
+            if UIImage(named: imageName) != nil {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: size, height: size)
+                    .clipShape(Circle())
+            } else {
+                Image(systemName: "person.crop.circle")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: size, height: size)
+                        .clipShape(Circle())
+                        .foregroundColor(Color(.systemGray4))
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    ZStack{
+        CircularImage(imageName: "WhiteRippleLogo", size: 64)
+    }
+    .background(.darkBG)
+    
+}
